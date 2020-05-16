9f6c6b90ef8fe0a01abe91e4e0b7e19f09752a70
Ihor <igornepipenko@gmail.com>
2020-05-16 12:56:41 +0300

    Feature: from js to ts  [ref:no-ref]

---
+++ index.ts
@@ -0,0 +1,32 @@
+Object.defineProperty(window, 'MySweetApp', {value: 'v1.0.0', writable: true});
+
+function deliveryMethod() {
+    // TODO some logic
+    return 'overnight';
+}
+
+function shipWeight() {
+    const el = document.getElementById('weight');
+    if (!el) {
+        return 0;
+    }
+    return parseInt(el.innerHTML, 10);
+}
+
+function sendUpdates(emailAddr: string | string[]) {
+    function sendEmail(addr: string) {
+        console.log(`Shipping to ${addr} via ${deliveryMethod() || 'standard'} delivery`);
+
+        if (shipWeight() > 100) {
+            console.log('WARNING: Oversize package');
+        }
+    }
+
+    if (Array.isArray(emailAddr)) {
+        emailAddr.forEach((val) => {
+            sendEmail(val.trim());
+        });
+    } else {
+        sendEmail(emailAddr.trim());
+    }
+}
