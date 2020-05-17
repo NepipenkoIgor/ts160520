a0cd88e0688f4268a89a24a06acd7c953b914034
Ihor <igornepipenko@gmail.com>
2020-05-17 12:50:08 +0300

    Feature: functions  [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,76 @@
-import { generateMenu } from './menu';
-import { list } from './menu/data';
+// function average(a: number, b: number, c: number): string {
+//     const avg: number = (a + b + c) / 3;
+//     return `Avg is ${avg}`;
+// }
 
-// declare function vk(selector: string): any;
+// function average(a: number, b?: number, c?: number): string {
+//     if (b === undefined) {
+//         b = 0;
+//     }
+//     if (c === undefined) {
+//         c = 0;
+//     }
+//     const avg: number = (a + b + c) / 3;
+//     return `Avg is ${avg}`;
+// }
 
-$('.menu')
-    .html(generateMenu(list))
-    .on('click', (e: Event) => {
-        const el: HTMLElement = e.target as HTMLElement;
-        if (!el.classList.contains('title')) {
-            return;
+// function average(a: number, b: number = 0, c: number = 0): string {
+//     const avg: number = (a + b + c) / 3;
+//     return `Avg is ${avg}`;
+// }
+// type sn = string | number
+//
+// function isString(item: sn): item is any[] {
+//     return typeof item === 'string';
+// }
+//
+// function average(...items: sn[]): string {
+//     let total: number = 0;
+//     for (const item of items) {
+//         if (isString(item)) {
+//             total += Number(item);
+//             continue;
+//         }
+//         total += item;
+//     }
+//     const avg: number = total / items.length;
+//     return `Avg is ${avg}`;
+// }
+
+type sn = string | number
+
+function isString(item: sn): item is string {
+    return typeof item === 'string';
+}
+
+
+function average(a: string, b: string): string;
+function average(a: string, b: number, c: number): string;
+function average(a: number, b: string): string;
+function average(...items: sn[]): string {
+    let total: number = 0;
+    for (const item of items) {
+        if (isString(item)) {
+            total += Number(item);
+            continue;
         }
-        const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
-        parentLi.classList.toggle('menu-open');
-    });
+        total += item;
+    }
+    const avg: number = total / items.length;
+    return `Avg is ${avg}`;
+}
 
 
-// const navMenu: HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
-// navMenu.innerHTML = generateMenu(list);
-// navMenu.addEventListener('click', (e) => {
-//     const el: HTMLElement = e.target as HTMLElement;
-//     if (!el.classList.contains('title')) {
-//         return;
-//     }
-//     const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
-//     parentLi.classList.toggle('menu-open');
-// });
+average('1', '2');
+average('1', 1, 1);
+average(1, 's');
 
+average(1);
+average(1, 2);
+average(1, '2', 3);
+average(1, '2', 3, 45);
+average(1, 2, 3, 45);
+average(1, 2, 3, 45, 34, 44, 4, 4, 4);
+const n: number = average(1, 2, 3);
 
-// import * as React from 'react';
-//
-// export interface Props {
-//     name: string,
-//     onSearch: (text: string) => void
-// }
-//
-// class Dropdown extends React.Component<Props> {
-//     constructor() {
-//         this.props.
-//     }
-// }
+let arr: Array
